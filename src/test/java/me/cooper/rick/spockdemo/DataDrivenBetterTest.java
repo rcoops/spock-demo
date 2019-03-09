42143ff7b474709e81a1432e2327d45a9d194401
rcoops <R.P.Cooper1@edu.salford.ac.uk>
2019-03-09 18:05:53 +0000

    cool stuff

--- src/test/java/me/cooper/rick/spockdemo/DataDrivenBetterTest.java
+++ src/test/java/me/cooper/rick/spockdemo/DataDrivenBetterTest.java
@@ -1,19 +1,25 @@
 package me.cooper.rick.spockdemo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import java.util.stream.Stream;
 
 import org.junit.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.runner.RunWith;
 
 import junitparams.JUnitParamsRunner;
+import junitparams.NamedParameters;
 import junitparams.Parameters;
 
 @RunWith(JUnitParamsRunner.class)
 public class DataDrivenBetterTest {
 
-  private static final CrapCalc crapCalc = new CrapCalc();
+  private static final CrapCalc calc = new CrapCalc();
 
   @CsvSource({
       "1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  10",
@@ -30,10 +36,33 @@ public class DataDrivenBetterTest {
   @ParameterizedTest(name = "{0} + {1} + {2} + {3} + {4} + {5} + {6} + {7} + {8} + {9} = {10}")
   public void addAll_givenArrayOfInputs_addsThemAllUp(int x, int y, int z, int a, int b, int c, int d, int e, int f,
       int g, int expectedOutput) {
-    assertEquals(crapCalc.addAll(x, y, z, a, b, c, d, e, f, g), expectedOutput);
+    assertEquals(calc.addAll(x, y, z, a, b, c, d, e, f, g), expectedOutput);
   }
 
+  @MethodSource("params")
+  @ParameterizedTest(name = "{0} + {1} + {2} + {3} + {4} + {5} + {6} + {7} + {8} + {9} = {10}")
+  public void addAll_givenArrayOfInputs_addsThemAllUpMethodProvider(int x, int y, int z, int a, int b, int c, int d,
+      int e, int f,
+      int g, int expectedOutput) {
+    assertEquals(calc.addAll(x, y, z, a, b, c, d, e, f, g), expectedOutput);
+  }
 
+  private static Stream<Arguments> params() {
+    return Stream.of(
+        //@formatter:off
+        arguments(1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  10 ),
+        arguments(2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  20 ),
+        arguments(3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  30 ),
+        arguments(4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  40 ),
+        arguments(5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  50 ),
+        arguments(6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  60 ),
+        arguments(7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  70 ),
+        arguments(8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  80 ),
+        arguments(9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  90 ) ,
+        arguments(10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 100)
+        //@formatter:on
+    );
+  }
 
   @Test
   @Parameters({
@@ -48,9 +77,46 @@ public class DataDrivenBetterTest {
       "9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  90",
       "10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 100"
   })
-  public void addAll_givenArrayOfInputs_addsThemAllUpParams(int x, int y, int z, int a, int b, int c, int d, int e, int f,
+  public void addAll_givenArrayOfInputs_addsThemAllUpParams(int x, int y, int z, int a, int b, int c, int d, int e,
+      int f,
       int g, int expectedOutput) {
-    assertEquals(crapCalc.addAll(x, y, z, a, b, c, d, e, f, g), expectedOutput);
+    assertEquals(calc.addAll(x, y, z, a, b, c, d, e, f, g), expectedOutput);
+  }
+
+  @Test
+  @Parameters(named = "params")
+  public void addAll_givenArrayOfInputs_addsThemAllUpParamsNamed(int x, int y, int z, int a, int b, int c, int d,
+      int e, int f, int g, int expectedOutput) {
+    assertEquals(calc.addAll(x, y, z, a, b, c, d, e, f, g), expectedOutput);
+  }
+
+  @Test
+  @Parameters
+  public void addAll_givenArrayOfInputs_addsThemAllUpParamsProvider(int x, int y, int z, int a, int b, int c, int d,
+      int e, int f, int g, int expectedOutput) {
+    assertEquals(calc.addAll(x, y, z, a, b, c, d, e, f, g), expectedOutput);
+  }
+
+  private static Object[] parametersForAddAll_givenArrayOfInputs_addsThemAllUpParamsProvider() {
+    return namedParams();
+  }
+
+  @NamedParameters("params")
+  private static Object[] namedParams() {
+    return new Object[] {
+        //@formatter:off
+        new Object[] { 1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  10  },
+        new Object[] { 2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  20  },
+        new Object[] { 3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  30  },
+        new Object[] { 4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  40  },
+        new Object[] { 5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  50  },
+        new Object[] { 6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  60  },
+        new Object[] { 7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  70  },
+        new Object[] { 8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  80  },
+        new Object[] { 9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  90  } ,
+        new Object[] { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 100 }
+        //@formatter:on
+    };
   }
 
 }
--- src/test/java/me/cooper/rick/spockdemo/DataDrivenCsv5Test.java
+++ src/test/java/me/cooper/rick/spockdemo/DataDrivenCsv5Test.java
@@ -2,23 +2,34 @@ package me.cooper.rick.spockdemo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.runner.RunWith;
 
+import junitparams.FileParameters;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
+import junitparams.mappers.CsvWithHeaderMapper;
 
+@RunWith(JUnitParamsRunner.class)
 public class DataDrivenCsv5Test {
 
-  private static final CrapCalc crapCalc = new CrapCalc();
+  private static final CrapCalc calc = new CrapCalc();
 
   @CsvFileSource(resources = "/test-data.csv", numLinesToSkip = 1)
   @ParameterizedTest(name = "{0} + {1} + {2} + {3} + {4} + {5} + {6} + {7} + {8} + {9} = {10}")
-  public void addAll_givenArrayOfInputs_addsThemAllUp(int x, int y, int z, int a, int b, int c, int d, int e, int f,
-      int g, int expectedAnswer) {
-    assertEquals(crapCalc.addAll(x, y, z, a, b, c, d, e, f, g), expectedAnswer);
+  public void addAll_givenArrayOfInputs_addsThemAllUpWithJUnit5(int x, int y, int z, int a, int b, int c, int d, int e,
+      int f, int g, int expectedAnswer) {
+    assertEquals(calc.addAll(x, y, z, a, b, c, d, e, f, g), expectedAnswer);
+  }
+
+  @Test
+  @FileParameters(value = "src/test/resources/test-data.csv", mapper = CsvWithHeaderMapper.class)
+  public void addAll_givenArrayOfInputs_addsThemAllUpWithJUnitParams(final int x, final int y, final int z, final int a,
+      final int b, final int c, final int d, final int e, final int f, final int g, final int expectedAnswer) {
+    Assert.assertEquals(calc.addAll(x, y, z, a, b, c, d, e, f, g), expectedAnswer);
   }
 }
